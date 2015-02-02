e84cd9484deee056c9fa232ccc0ae9e6d88f4f45
Iikka Winter <iikka.winter@sc5.io>
2015-02-02 22:21:27 +0200

    Polish part divider styles.

--- src/app/controllers/part-divider-controller.js
+++ src/app/controllers/part-divider-controller.js
@@ -1,8 +1,9 @@
 angular.module('HY')
-  .controller('PartDividerController', function($scope, $stateParams, $controller) {
+  .controller('PartDividerController', function($scope, $stateParams, $controller, Utils) {
     angular.extend(this, $controller('MainController', {$scope: $scope}));
 
     $scope.text = 'parts.' + $stateParams.partId.toUpperCase();
+    $scope.partNo = Utils.romanize(parseInt($stateParams.partId.replace('part', '')));
     $scope.addClasses = 'part-divider';
 
   });
